c863ff9b7f0f1b06d3cf63dd0acd1cddad776cc6
priddhesh <109747774+priddhesh@users.noreply.github.com>
2023-01-07 19:12:11 +0530

    Update Navbar.js

--- src/compenents/Navbar.js
+++ src/compenents/Navbar.js
@@ -6,14 +6,14 @@ function Navbar() {
         <>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/home" style={{ fontFamily: "'Andika', sans-serif"}}>Vatavaran</a>
+                    <a className="navbar-brand" href="/" style={{ fontFamily: "'Andika', sans-serif"}}>Vatavaran</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav">
                             <li className="nav-item">
-                                <a className="nav-link active" style={{ fontWeight:"bold"}} aria-current="page" href="/home">Home</a>
+                                <a className="nav-link active" style={{ fontWeight:"bold"}} aria-current="page" href="/">Home</a>
                             </li>
                         </ul>
                     </div>
