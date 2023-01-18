8967143edb2a52c6a98d2465d92fd9f4987870e6
priddhesh <109747774+priddhesh@users.noreply.github.com>
2023-01-18 22:56:13 +0530

    Update Graph.js

--- src/compenents/Graph.js
+++ src/compenents/Graph.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import CanvasJSReact from './canvasjs.react';
-var CanvasJS = CanvasJSReact.CanvasJS;
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 class Graph extends Component {
